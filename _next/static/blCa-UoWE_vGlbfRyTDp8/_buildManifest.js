d641c83ea2b30c44311527c3148f21388a4a7db9
s1f102103707 <s1f102103707@users.noreply.github.com>
2023-06-30 14:44:37 +0000

    deploy: d40ab133e1d40d76c3fe5dec5b07646a85004e20

--- _next/static/blCa-UoWE_vGlbfRyTDp8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cfd1253c8d2ff044.css","static/chunks/pages/index-4b8ed65cb1b16b18.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
