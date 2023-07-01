9e1a45b74ff86dd8eec0bc24bf1ec6119f88c35b
s1f102103707 <s1f102103707@inaid.org>
2023-07-01 23:47:04 +0900

    色変え2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,6 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
-const dx = [0, 1, 1, 1, 0, -1, -1, -1];
-const dy = [-1, -1, 0, 1, 1, 1, 0, -1];
-
-function findLine(board: number[][], x: number, y: number, color: number, dir: number) {
-  let i = x + dx[dir];
-  let j = y + dy[dir];
-  while (i >= 0 && i < 8 && j >= 0 && j < 8) {
-    if (board[j][i] === 0) {
-      return null;
-    } else if (board[j][i] === color) {
-      return { i, j };
-    }
-    i += dx[dir];
-    j += dy[dir];
-  }
-  return null;
-}
-
-function flipLine(board: number[][], x: number, y: number, i: number, j: number) {
-  while (x !== i || y !== j) {
-    board[y][x] = 3 - board[y][x];
-    x += Math.sign(i - x);
-    y += Math.sign(j - y);
-  }
-}
-
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
@@ -42,13 +16,31 @@ const Home = () => {
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    //for (let dx = -1; dx <= 1; dx++) {
-    //for (let dy = -1; dy <= 1; dy++) {
-    //if (dx === 0 && dy === 0) continue;
-    //newBoard[y][x] = turnColor;
-    //}
-    //}
+    const dx = [0, 1, 1, 1, 0, -1, -1, -1];
+    const dy = [-1, -1, 0, 1, 1, 1, 0, -1];
 
+    function findLine(board: number[][], x: number, y: number, turnColor: number, dir: number) {
+      let i = x + dx[dir];
+      let j = y + dy[dir];
+      while (i >= 0 && i < 8 && j >= 0 && j < 8) {
+        if (board[j][i] === 0) {
+          return null;
+        } else if (board[j][i] === turnColor) {
+          return { i, j };
+        }
+        i += dx[dir];
+        j += dy[dir];
+      }
+      return null;
+    }
+
+    function flipLine(board: number[][], x: number, y: number, i: number, j: number) {
+      while (x !== i || y !== j) {
+        board[y][x] = 3 - board[y][x];
+        x += Math.sign(i - x);
+        y += Math.sign(j - y);
+      }
+    }
     if (newBoard[y][x] === 0) {
       //if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
       //newBoard[y][x] = turnColor;
@@ -63,11 +55,6 @@ const Home = () => {
       }
       setTurnColor(3 - turnColor);
     }
-    //else if (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) {
-    //newBoard[y][x] = turnColor;
-    //setTurnColor(3 - turnColor);
-    //}
-    //}
     setBoard(newBoard);
   };
 
